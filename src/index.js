08251cb2a8faa58203b32d07e5758004d2784b95
Andrew Jones <aej11@outlook.com>
2020-10-30 11:02:04 -0400

    package.json cleanup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-export default class DemoPlugin {
+const path = require('path');
+const fs = require('fs');
+
+export default class TotalConfigPlugin {
   constructor(options) {
     this.options = options;
+    this.pushedPaths = [];
   }
 
   apply(compiler) {
-    compiler.hooks.done.tap('DemoPlugin', () => {
-      console.log('\nHello world\n');
+    compiler.hooks.afterEnvironment.tap('TotalConfigPlugin', () => {
+      const totalConfig = require(`${path.resolve()}/total.config`);
+
+      for (const configKey in totalConfig) {
+        const newConfigFileSource =
+          `module.exports = ${JSON.stringify(totalConfig[configKey])}`;
+          
+        fs.writeFile(`${configKey}.config.js`, newConfigFileSource, () => {
+          this.pushedPaths.push(`${configKey}.config.js`);
+        });
+      }
+    });
+
+    compiler.hooks.afterEmit.tap('TotalConfigPlugin', () => {
+      for (const filepath of this.pushedPaths) {
+        fs.unlinkSync(`${path.resolve()}/${filepath}`);
+      }
     });
   }
 }
